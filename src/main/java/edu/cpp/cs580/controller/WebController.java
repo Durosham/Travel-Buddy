e1691eb96f3de982f1f7e3a2314733c568fd2562
Hesham Darwish <hesham.darwish8@gmail.com>
2016-02-15 15:40:14 -0800

    Just adding some stuff and linking the index page.
    Adding Facebook App id and secret.

--- src/main/java/edu/cpp/cs580/controller/WebController.java
+++ src/main/java/edu/cpp/cs580/controller/WebController.java
@@ -28,6 +28,16 @@ import edu.cpp.cs580.data.InfoData;
 import edu.cpp.cs580.data.User;
 import edu.cpp.cs580.data.provider.UserManager;
 
+import javax.inject.Inject;
+
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * This is the controller used by Spring framework.
